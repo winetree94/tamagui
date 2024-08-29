0652215208dd7b66051964ec81a6f0b6513c1a04
winetree94 <winetree94@gmail.com>
2024-08-29 17:39:53 +0900

    update

---
+++ packages/app/src/types.d.ts
@@ -0,0 +1,6 @@
+// export type Conf = typeof config;
+
+// declare module 'tamagui' {
+//   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+//   interface TamaguiCustomConfig extends Conf {}
+// }
